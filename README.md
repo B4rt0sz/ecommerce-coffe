0e870e89e6b3d2956bde25bdbf93d9462690e8ef
Bart <bartosz.szucko@gmail.com>
2021-11-11 21:12:28 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+E-commerce coffe shop.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/index.scss'
+
+function App() {
+
+  return (
+    <>
+      <div>EGO COFFE Site</div>
+    </>
+  )
+}
+
+export default App
