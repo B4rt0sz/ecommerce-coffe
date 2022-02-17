e8a106f12f24f0ff596b8ef2bff3acd841f3a8d7
Bart <bartosz.szucko@gmail.com>
2022-02-17 18:58:12 +0100

    add firebase cloudfuntions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,46 +1,19 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     node: true,
     es6: true,
   },
   parser: '@babel/eslint-parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'prettier',
-    'prettier/react',
-  ],
+  extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     ecmaFeatures: {
       jsx: true,
     },
     sourceType: 'module',
-  },
-  plugins: ['@babel', 'import', 'react', 'react-hooks', 'prettier'],
-  rules: {
-    'no-var': 'error',
-    'array-callback-return': 'error',
-    'consistent-return': 'error',
-    'babel/no-invalid-this': 'error',
-    'import/no-duplicates': 'error',
-    'import/no-unresolved': 'error',
-    'import/named': 'error',
-    'prettier/prettier': 'error',
-    'react/no-typos': 'error',
-    'react/no-unused-state': 'error',
-    'react/jsx-no-bind': 'error',
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
-  settings: {
-    react: {
-      pragma: 'React',
-      version: 'detect',
-      flowVersion: '0.53.1',
-    },
+    requireConfigFile: 'false',
+    babelOptions: { configFile: './.babelrc' },
   },
 }
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/ego-coffee-ecommerce/us-central1/api',
+  baseURL: 'https://us-central1-ego-coffee-ecommerce.cloudfunctions.net/api',
+  // 'http://localhost:5001/ego-coffee-ecommerce/us-central1/api',
 })
 
 export default instance
